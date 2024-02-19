5bf37bf70b32c73ffe7c51c077d6510f66dd4463
lotsik <lotsik@mail.ru>
2024-02-19 22:08:13 +0300

    Update Guid.php

--- src/Util/Guid.php
+++ src/Util/Guid.php
@@ -64,7 +64,7 @@ class Guid
     private static function factory(): UuidFactory
     {
         if (null === self::$factory) {
-            self::$factory = new UuidFactory(new FeatureSet(true));
+            self::$factory = new UuidFactory(new FeatureSet(false));
         }
 
         return self::$factory;
